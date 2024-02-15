4f2a1cefd785f860821817ee52778247b7cf998b
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-02-15 09:50:31 -0600

    update(View Categories): created ViewCategories.vue && started on page

--- src/router.js
+++ src/router.js
@@ -9,6 +9,8 @@ import ViewTutorial from "./views/ViewTutorial.vue";
 import AddLesson from "./views/AddLesson.vue";
 import EditLesson from "./views/EditLesson.vue";
 
+import ViewCategories from "./views/ViewCategories.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -50,6 +52,12 @@ const router = createRouter({
       path: "/editLesson/:tutorialId/:lessonId",
       name: "editLesson",
       component: EditLesson,
+      props: true,
+    },
+    {
+      path: "/viewCategories/",
+      name: "viewCategories",
+      component: ViewCategories,
       props: true,
     },
   ],
---
+++ src/views/ViewCategories.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+</script>
+
+<template>
+<div  class="ml-10 mt-7">
+    <div style="font-size: x-large;">
+        Category Lookup
+    </div>
+</div>
+</template>
