3640da86b27f30e9bafde6ce487e9403396a2b15
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-02-19 15:53:04 -0600

    update(view categories): categories are now viewable by activity

--- src/views/ViewCategories.vue
+++ src/views/ViewCategories.vue
@@ -1,82 +1,96 @@
 <script setup>
-import { ref, onMounted } from "vue";
-import { useRouter } from "vue-router";
-import CategoryServices from "../services/categorySerices"
- 
-const router = useRouter();
+  import { ref, onMounted, computed } from "vue";
+  import CategoryServices from "../services/categorySerices";
+  
+  const categories = ref([]);
+  const tab = ref("visible");
+  const newCategory = ref("");
 
-const categories = ref([]);
-const catNames = ref([])
-
-async function getCategory() {
-    await CategoryServices.getAll()
-        .then((response) => {
-            console.log(response);
-            categories.value= response.data;
-        })
-        .catch((err) => {
-                console.log(err);
-        })
-}
-
-onMounted(async() => {
+  const headers = ref([
+    { title: 'Name', value: "catName" },
+    { title: 'Action', align: "end"}
+  ])
+  
+  async function getCategory() {
+    try {
+      const response = await CategoryServices.getAll();
+      categories.value = response.data;
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  }
+  
+  const currentCategories = computed(() => {
+    return tab.value === "visible"
+      ? categories.value.filter(c => c.active)
+      : categories.value.filter(c => !c.active);
+  });
+  
+  function toggleTab(selectedTab) {
+    tab.value = selectedTab;
+  }
+  
+  async function saveCategory() {
+    // TODO Implement saving of new category using CategoryServices
+    // & saves new category
+  }
+  
+  function viewCategory(category) {
+    // TODO Implement viewing of category details
+    // & view single category
+    // ? not necessary if clickable?
+    // * eli and trina want clickable
+  }
+  
+  function hideCategory(category) {
+    // TODO Implement hiding of category
+    // & make catgory now hidden
+    // ^ will need api calls
+  }
+  
+  onMounted(async () => {
     await getCategory();
-})
+  });
 </script>
-
+  
 <template>
-<div  class="ma-15 mt-7">
-
-    <!-- Page Title -->
-    <div style="font-size: x-large;">
-        Category Lookup
-    </div>
-
-    <!-- Category Data Table -->
-    <!-- TODO Add Table functionality -->
-    <v-dataTable class="display">
-        <thead>
-            <tr>
-                <th style="font-weight: bolder;">Category Name</th>
-                <th style="font-weight: bolder; text-align: right;">View</th>
-            </tr>
-            <tr v-for="c in categories">
-                <th>
-                    {{ c.catName }}
-                </th>
-                <th>
-                    <div style="display: flex; justify-content: end;">
-                    <td class="pr-5">
-                        <v-btn color="blue" elevation="1">view</v-btn>
-                    </td>
-                    <td class="pl-5">
-                        <v-btn color="gray" elevation="1">hide</v-btn>
-                    </td>
-                    </div>
-                </th>
-            </tr>
-        </thead>
-    </v-dataTable>
-
-    <!-- Add Category Display -->
-    <!-- TODOAdd API functionality -->
-    <div class="pt-10">
-    <v-card color="gray" class="pa-4">
-        <v-row align="center" class="mr-2">
+    <div class="ma-15 mt-7">
+      <!-- Page Title -->
+      <div style="font-size: x-large;">Category Lookup</div>
+  
+      <!-- Category Data Table -->
+      <v-tabs color="blue">
+        <v-tab :value="tab" @click="toggleTab('visible')">Visible</v-tab>
+        <v-tab :value="tab" @click="toggleTab('hidden')">Hidden</v-tab>
+      </v-tabs>
+  
+      <v-data-table :items="currentCategories" class="display" :headers="headers">
+        <template v-slot:item="{ item }">
+          <tr>
+            <td>{{ item.catName }}</td>
+            <td class="text-right">
+                <!-- View button -->
+                <v-btn elevation="1" size="small" color="blue" @click="viewCategory(item)" class="mr-10">View</v-btn>
+                <!-- Hide button -->
+                <v-btn elevation="1" size="small" color="gray" @click="hideCategory(item)">Hide</v-btn>
+            </td>
+          </tr>
+        </template>
+      </v-data-table>
+  
+      <!-- Add Category Display -->
+      <div class="pt-10">
+        <v-card color="gray" class="pa-4">
+          <v-row align="center" class="mr-2">
             <v-col cols="4">
-                <v-text-field label="Category Name"></v-text-field>
+              <v-text-field v-model="newCategory" label="Category Name"></v-text-field>
             </v-col>
-            <v-col cols="8" class="text-right"> 
-                <v-btn color="blue">Save</v-btn>
+            <v-col cols="8" class="text-right">
+              <v-btn color="blue" @click="saveCategory">Save</v-btn>
             </v-col>
-        </v-row>
-    </v-card>
-</div>
-
-</div>
-
-
-<!-- TODO data table of 2 tabs: visible and hidden -->
-<!-- TODO List of category name on the left and view and hide buttons on the right -->
-
-</template>
+          </v-row>
+        </v-card>
+      </div>
+    </div>
+  </template>
+  
