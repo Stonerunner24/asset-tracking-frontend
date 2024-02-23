2547a6ca16e8d6dde70c27d637ca21de096b5062
trinajkenyon <trina.kenyon@eagles.oc.edu>
2024-02-23 14:30:56 -0600

    Add: combobox with populated category names

--- src/views/AddModel.vue
+++ src/views/AddModel.vue
@@ -1,6 +1,25 @@
 <script setup>
+import { ref, onMounted, computed } from "vue";
 import Sidebar from "../components/SideBar.vue";
+import modelServices from "../services/modelServices";
+import categoryServices from "../services/categorySerices";
+import router from "../router";
 
+const categories = ref([]);
+const categoryNames = computed(() => categories.value.map(category => category.catName));
+
+
+async function getCategories() {
+    try {
+        const response = await categoryServices.getAll();
+        categories.value = response.data;
+    } catch (error) {
+        console.error("Error fetching categories:", error);
+    }
+}
+onMounted(async () => {
+    await getCategories();
+});
 </script>
 <template>
     <v-container>
@@ -8,7 +27,8 @@ import Sidebar from "../components/SideBar.vue";
     </v-container>
     <div class="ma-15 mt-7">
         <!-- Page Title -->
-        <div style="font-size: x-large;">View Model</div> <!--This ought to be a h1 or something?-->
-        <v-combobox></v-combobox>
+        <div style="font-size: x-large;">Add Model</div> <!--This ought to be a h1 or something?-->
+        <v-combobox clearable label="Category" :items="categoryNames">
+        </v-combobox>
     </div>
 </template>
