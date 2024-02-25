18527c4348e01315b7c7f40d2dcf71ac11ec8f86
theshaggy5000 <nickdrebot@gmail.com>
2024-02-25 15:40:07 -0600

    added fieldServices.js file with get all, get for id, create, update, delete and delete all functions

---
+++ src/services/fieldServices.js
@@ -0,0 +1,24 @@
+import apiClient from "./services";
+const { authenticate } = require("../authorization/authorization.js");
+
+export default {
+    //get all fields
+    getAll(){
+        return apiClient.get("/field");
+    },
+    getAllForId(id){
+        return apiClient.get(`/field/${id}`);
+    },
+    create(data){
+        return apiClient.post("/field", data);
+    },
+    update(id, data){
+        return apiClient.put(`/field/${id}`, data);
+    },
+    delete(id){
+        return apiClient.delete(`/field/${id}`);
+    },
+    deleteAll(){
+        return apiClient.delete("/field");
+    }
+};
