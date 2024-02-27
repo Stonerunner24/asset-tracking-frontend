aee7317b4fe235b58d0019b5f5c06632395d5c1b
trinajkenyon <trina.kenyon@eagles.oc.edu>
2024-02-26 22:32:04 -0600

    Aesthetics

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,23 @@
 <script setup>
 import SocialLogin from "../components/SocialLogin.vue";
+import ocLogo from "/oc-logo-red.svg";
 </script>
 
 <template>
+  <v-app-bar app color="primary" height="80">
+    <v-img
+          class="mx-2"
+          :src=ocLogo
+          height="80"
+          width="80"
+          contain
+        ></v-img>
+        <v-toolbar-title class="title">
+        Asset Tracking
+      </v-toolbar-title>
+      <v-spacer/>
+      <v-spacer/>
+  </v-app-bar>
   <v-container>
     <h1>Welcome to OC Asset Tracking</h1>
     <text-subtitle-1>Please log in with OC credentials to continue</text-subtitle-1>
