e35070d5cbe3e3bc9b60de61253aa814ceaca748
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-02-26 23:18:29 -0600

    update(home): temp home quicklinks made

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,6 +1,44 @@
 <script setup>
+import router from "../router";
+
+function goToViewCategories(){
+    router.push("/viewCategories")
+}
+function goToViewTypes(){
+    router.push("/")
+}
+function goToViewModels(){
+    router.push("/")
+}
+function goToViewItems(){
+    router.push("/itemView")
+}
 
 </script>
 <template>
+    <div class="pb  -2 pt-4">
+        <v-card flat color="gray" class="pa-4" style="font-size: larger;">Welcome!</v-card>
+    </div>
 
+    <div class="pt-10">
+        <v-row>
+            <v-col cols="12" md="6">
+                <div>Quicklinks</div>
+                <v-card color="gray" flat>
+                    <div class="pa-5">
+                        <v-btn flat block color="silver" @click="goToViewCategories()">View Categories</v-btn>
+                    </div>
+                    <div class="pa-5">
+                        <v-btn flat block color="silver" @click="goToViewTypes">View Types</v-btn>
+                    </div>
+                    <div class="pa-5">
+                        <v-btn flat block color="silver" @click="goToViewModels">View Models</v-btn>
+                    </div>
+                    <div class="pa-5">
+                        <v-btn flat block color="silver" @click="goToViewItems">View Items</v-btn>
+                    </div>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
 </template>
