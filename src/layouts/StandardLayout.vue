0877e7bf3a353aef35a723ac8960b3e00d3b4c8a
trinajkenyon <trina.kenyon@eagles.oc.edu>
2024-02-26 22:24:36 -0600

    Add: layouts to hold menu and side bars

---
+++ src/layouts/StandardLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import MenuBar from "../components/MenuBar.vue";
+  import SideBar from "../components/SideBar.vue";
+</script>
+<template>
+    <div>
+      <MenuBar />
+      <SideBar />
+      <v-container>
+        <router-view />
+      </v-container>
+    </div>
+</template>
+  
--- src/router.js
+++ src/router.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+// import pages
 import Login from "./views/Login.vue";
+import Homepage from "./views/Homepage.vue";
 import ItemLookup from "./views/ItemLookup.vue";
 import ItemView from "./views/ItemView.vue";
 import TypeView from "./views/TypeView.vue"
 import ViewCategory from "./views/ViewCategory.vue";
 import ViewCategories from "./views/ViewCategories.vue";
+
+
+// import layouts
+import StandardLayout from "./layouts/StandardLayout.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,35 +23,49 @@ const router = createRouter({
       component: Login,
     },
     {
-      path: "/itemlookup",
-      alias: "/itemlookup",
-      name: "itemlookup",
-      component: ItemLookup
-    },
-    {
-      path:"/itemview/:id",
-      alias: "/itemview/:id",
-      name: "itemview",
-      component: ItemView
-    },
-    {
-      path: "/viewCategories/",
-      name: "viewCategories",
-      component: ViewCategories,
-      props: true,
-    },
-    {
-      path: "/viewCategory/:id",
-      name: "viewCategory",
-      component: ViewCategory,
-      props: true,
-    },
-    {
-      path: "/typeview/:id",
-      name: "typeview",
-      component: TypeView,
-      props: true,
-    },
+      // Parent path to all standardLayout pages
+      // It contains the sidebar and menubar
+      path: "/home",
+      component: StandardLayout,
+      children: [
+        {
+          path: "",
+          name: "homepage",
+          component: Homepage
+        },
+        {
+          path: "/itemlookup",
+          alias: "/itemlookup",
+          name: "itemlookup",
+          component: ItemLookup
+        },
+        {
+          path:"/itemview/:id",
+          alias: "/itemview/:id",
+          name: "itemview",
+          component: ItemView
+        },
+        {
+          path: "/viewCategories/",
+          name: "viewCategories",
+          component: ViewCategories,
+          props: true,
+        },
+        {
+          path: "/viewCategory/:id",
+          name: "viewCategory",
+          component: ViewCategory,
+          props: true,
+        },
+        {
+          path: "/typeview/:id",
+          name: "typeview",
+          component: TypeView,
+          props: true,
+        },
+      ]
+    }
+    
   ],
 });
 
