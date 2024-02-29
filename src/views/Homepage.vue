7cabe5f6f57a84c93aa670f77e47417cc4acce49
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-02-28 21:36:56 -0600

    update(parse router): quicklink system now parses the pages in the router

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,6 +1,13 @@
 <script setup>
 import router from "../router";
+import { onMounted, ref, computed } from "vue";
 
+const routes = router.options.routes;
+const routePaths = ref([]);
+const routeNames = ref([]);
+function changePage(route){
+    router.push(route);
+}
 function goToViewCategories(){
     router.push("/viewCategories")
 }
@@ -14,18 +21,37 @@ function goToViewItems(){
     router.push("/itemlookup")
 }
 
+const setPages = async() => {
+    const homeRoute = routes.find(route => route.path === '/home');
+    if (homeRoute && homeRoute.children) {
+        for (const childRoute of homeRoute.children) {
+            routePaths.value.push(childRoute.path);
+            routeNames.value.push(childRoute.name);
+        }
+    }
+}
+
+onMounted (async() => {
+    await setPages();
+});
+
+
 </script>
 <template>
 <div class="ml-15 mt-5">
+    <div>{{ routeNames }}</div>
     <div class="pb-2 pt-4 mr-15">
         <v-card flat color="gray" class="pa-4" style="font-size: larger;">Welcome!</v-card>
     </div>
-
     <div class="pt-10">
+        <div>{{ pages }}</div>
         <v-row>
             <v-col cols="12" md="6">
                 <div>Quicklinks</div>
                 <v-card color="gray" flat>
+                    <div class="pa-5">
+                        <v-btn flat block color="silver"  @click="changePage()">View Categories</v-btn>
+                    </div>
                     <div class="pa-5">
                         <v-btn flat block color="silver" @click="goToViewCategories()">View Categories</v-btn>
                     </div>
