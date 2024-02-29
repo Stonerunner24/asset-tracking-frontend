1bf869c1d0db669b4528bd49f8ca6a11213201b4
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-02-28 22:30:03 -0600

    update(quicklinks buttons): the testing for how the quicklinks will function has proved positive

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -4,8 +4,10 @@ import { onMounted, ref, computed } from "vue";
 
 const routes = router.options.routes;
 const routePaths = ref([]);
-const routeNames = ref([]);
+const selectedPage = ref(null);
+
 function changePage(route){
+    console.log(route)
     router.push(route);
 }
 function goToViewCategories(){
@@ -25,8 +27,12 @@ const setPages = async() => {
     const homeRoute = routes.find(route => route.path === '/home');
     if (homeRoute && homeRoute.children) {
         for (const childRoute of homeRoute.children) {
-            routePaths.value.push(childRoute.path);
-            routeNames.value.push(childRoute.name);
+            const data = {
+                "path": childRoute.path,
+                "name": childRoute.name,
+            }
+            
+            routePaths.value.push(data);
         }
     }
 }
@@ -35,22 +41,26 @@ onMounted (async() => {
     await setPages();
 });
 
+const pages = computed(() => {
+    return routePaths.value.map(route => route.name);
+});
 
 </script>
 <template>
 <div class="ml-15 mt-5">
-    <div>{{ routeNames }}</div>
+    <div>{{ routePaths }}</div>
     <div class="pb-2 pt-4 mr-15">
         <v-card flat color="gray" class="pa-4" style="font-size: larger;">Welcome!</v-card>
     </div>
     <div class="pt-10">
-        <div>{{ pages }}</div>
+        <div>{{ routePaths.path }}</div>
         <v-row>
             <v-col cols="12" md="6">
                 <div>Quicklinks</div>
                 <v-card color="gray" flat>
                     <div class="pa-5">
-                        <v-btn flat block color="silver"  @click="changePage()">View Categories</v-btn>
+                        <v-combobox :items="routePaths" item-text="name" item-value="path" v-model="selectedPage"></v-combobox>
+                        <v-btn flat color="silver"  @click="changePage(selectedPage.path)">View Categories</v-btn>
                     </div>
                     <div class="pa-5">
                         <v-btn flat block color="silver" @click="goToViewCategories()">View Categories</v-btn>
