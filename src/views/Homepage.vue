1025900d5e90c8ffc4a85a915925b6df853b27af
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-03-02 13:01:19 -0600

    update(save quicklinks): api call to save quicklinks

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -9,6 +9,7 @@ const routePaths = ref([]);
 const selectedPage = ref(null);
 let user = Utils.getStore("user");
 const userQuickLinks = ref([]);
+let showAdd = true;
 
 function changePage(route){
     console.log(route)
@@ -32,12 +33,22 @@ const setPages = async() => {
 
 async function getQuickLinks() {
     try {
-        console.log(`user: ${user.userId}`);
         const response = await QuicklinkServices.getByUserId(user.userId);
         userQuickLinks.value = response.data;
-        console.log('User links:', userQuickLinks.value);
     } catch (error) {
         console.error('Error fetching User\'s quicklinks: ', error);
+    }
+}
+
+async function saveLink(){
+    try{
+        const data = {
+            name: selectedPage.value.name,
+            path: selectedPage.value.path,
+        }
+        await QuicklinkServices.create(data);
+    } catch (error) {
+
     }
 }
 
@@ -48,35 +59,47 @@ onMounted (async() => {
 
 </script>
 <template>
-<div class="ml-15 mt-5">
-    <div v-for="route in routePaths">
+    <div class="ml-15 mt-5">
+        <div v-for="route in routePaths">
+        </div>
+        <div class="pb-2 pt-4 mr-15">
+            <v-card flat color="gray" class="pa-4" style="font-size: larger;">Welcome, {{ user.fName }}!</v-card>
+        </div>
+        <div class="pt-10">
+            <v-row>
+                <v-col cols="12" md="6">
+                    <div>Quicklinks</div>
+                    <v-card color="gray" flat>
+                        <div class="pa-3">
+                            <v-btn v-for="p in userQuickLinks" flat @click="changePage(p.path)">{{ p.name }}</v-btn>
+                        </div>
+                        <div class="pa-3" v-if="userQuickLinks.length <= 5">
+                            <v-combobox 
+                                v-model="selectedPage" 
+                                :items="routePaths" 
+                                label="Select a Page" 
+                                item-title="name" 
+                                item-value="path"
+                                style="width: 20rem; height: 4rem; float: left;"
+                            ></v-combobox>
+                            <div class="pa-1" style="display: flex; justify-content: center;">
+                                <v-btn 
+                                flat size="small" 
+                                color="blue" 
+                                style="margin-right: 10px;"
+                                @click="saveLink()"
+                                >Save</v-btn>
+                                <v-btn 
+                                flat size="small"
+                                @click="cancelLink()"
+                                >Cancel</v-btn>
+                            </div>
+                        </div>
+                    
+                    </v-card>
+                </v-col>
+            </v-row>
+        </div>
     </div>
-    <div class="pb-2 pt-4 mr-15">
-        <v-card flat color="gray" class="pa-4" style="font-size: larger;">Welcome, {{ user.fName }}!</v-card>
-    </div>
-    <div class="pt-10">
-        <!-- <div>{{ routePaths.name }}</div> -->
-        <v-row>
-            <v-col cols="12" md="6">
-                <div>Quicklinks</div>
-                <v-card color="gray" flat>
-                    <div class="pa-3" >
-                        <v-btn v-for="p in userQuickLinks"flat  @click="changePage(p.path)">{{ p.name }}</v-btn>
-                    </div>
-                    <div class="pa-3" v-if="userQuickLinks.length <= 5" style="float: left;">
-                        <v-combobox 
-                        v-model="selectedPage" 
-                        :items="routePaths" 
-                        label="Select a Page" 
-                        item-title="name" 
-                        item-value="path"
-                        style="width: 20rem; height: 4rem;"
-                        ></v-combobox>                    
-                    </div>
-                
-                </v-card>
-            </v-col>
-        </v-row>
-    </div>
-</div>
-</template> 
+    </template>
+    
