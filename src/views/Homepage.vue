f7bd2d8372dbcd37076590a60b61ce0661be75aa
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-03-01 20:27:04 -0600

    update(combobox): combobox displays only viewable routes

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -18,14 +18,16 @@ const setPages = async() => {
     const homeRoute = routes.find(route => route.path === '/home');
     if (homeRoute && homeRoute.children) {
         for (const childRoute of homeRoute.children) {
-            const data = {
-                "path": childRoute.path,
-                "name": childRoute.meta ? childRoute.meta.qlname : childRoute.name,
+            if (childRoute.meta.qlviewable == true) {
+                const data = {
+                    "path": childRoute.path,
+                    "name": childRoute.meta ? childRoute.meta.qlname : childRoute.name,
+                }
+                routePaths.value.push(data);
             }
-            routePaths.value.push(data);
         }
     }
-    console.log(routePaths.name)
+    console.log('routes',routePaths.value)
 }
 
 async function getQuickLinks() {
@@ -47,6 +49,8 @@ onMounted (async() => {
 </script>
 <template>
 <div class="ml-15 mt-5">
+    <div v-for="route in routePaths">
+    </div>
     <div class="pb-2 pt-4 mr-15">
         <v-card flat color="gray" class="pa-4" style="font-size: larger;">Welcome, {{ user.fName }}!</v-card>
     </div>
@@ -56,10 +60,20 @@ onMounted (async() => {
             <v-col cols="12" md="6">
                 <div>Quicklinks</div>
                 <v-card color="gray" flat>
-                    <div v-if="userQuickLinks.length <= 5">
-                        <v-combobox :items="routePaths" item-text="name"></v-combobox>
+                    <div class="pa-3" >
+                        <v-btn v-for="p in userQuickLinks"flat  @click="changePage(p.path)">{{ p.name }}</v-btn>
                     </div>
-                    <v-btn v-for="p in userQuickLinks" @click="changePage(p.path)">{{ p.name }}</v-btn>
+                    <div class="pa-3" v-if="userQuickLinks.length <= 5" style="float: left;">
+                        <v-combobox 
+                        v-model="selectedPage" 
+                        :items="routePaths" 
+                        label="Select a Page" 
+                        item-title="name" 
+                        item-value="path"
+                        style="width: 20rem; height: 4rem;"
+                        ></v-combobox>                    
+                    </div>
+                
                 </v-card>
             </v-col>
         </v-row>
