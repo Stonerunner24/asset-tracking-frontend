deffc92c8c68e4b1cb7764543e2e2db4f7cb7251
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-03-07 09:30:44 -0600

    update(building lookup setup): setup building lookup page and api/services

--- src/router.js
+++ src/router.js
@@ -14,6 +14,7 @@ import ModelLookup from "./views/ModelLookup.vue";
 
 // import layouts
 import StandardLayout from "./layouts/StandardLayout.vue";
+import BuildingLookup from "./views/BuildingLookup.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -101,8 +102,21 @@ const router = createRouter({
         {
           path: "/modellookup",
           name: "modellookup",
-          component: ModelLookup
+          component: ModelLookup,
+          meta: {
+            qlname: "Model Lookup",
+            qlviewable: true,
+          }
         },
+        {
+          path: "/buildinglookup",
+          name: "buildinglookup",
+          component: BuildingLookup,
+          meta: {
+            qlname: "Building Lookup",
+            qlviewable: true,
+          }
+        }
       ]
     }
     
---
+++ src/views/BuildingLookup.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import BuildingServices from "../services/buildingServices";
+    import { ref, onMounted } from "vue";
+
+    const buildings = ref([]);
+
+    async function getBuildings() {
+        try {
+            const response = await BuildingServices.getAll();
+            buildings.value = response.data;
+            console.log(buildings)
+        } catch (error) {
+            console.error("Error fetching buildings:", error);
+        }
+    }
+
+    onMounted(async () => {
+        await getBuildings();
+    });
+</script>
+<template>
+    <div>{{ buildings }}</div>
+</template>
