31672ca8cf148609c7f607c66eaa5bd13a9e7e83
trinajkenyon <trina.kenyon@eagles.oc.edu>
2024-03-09 16:40:55 -0600

    updated typeServices to handle item and model typeFields

--- src/views/AddModel.vue
+++ src/views/AddModel.vue
@@ -13,6 +13,9 @@ const types = ref([]);
 const typeNames = computed(() => types.value.map(type => type.typeName));
 const activeType = ref();
 
+const modelFields = ref([]);
+const fieldValues = ref(Array.from({ length: modelFields.value.length }, () => ''));
+
 async function getCategories() {
     // This eventually needs to be altered to only fetch the categories the user is assigned to
     try {
@@ -41,6 +44,18 @@ async function getTypesForCategory(categoryId) {
     }
 }
 
+async function getFieldsForType(typeId) {
+    console.log("In getFieldsForType");
+    try {
+        const response = await typeServices.getAllFields(typeId);
+        console.log(response.data);
+        modelFields.value = response.data.filter(typeField => typeField.isItem == false);
+        console.log(modelFields.value);
+    } catch (error) {
+        console.error("Error fetching typeFields: ", error);
+    }
+}
+
 async function changeCategory(clearType) {
     
     // 1: category is reset. Result: type is unfiltered
@@ -62,6 +77,9 @@ async function changeCategory(clearType) {
 async function changeType() {
     // Get full active type object
     let type = types.value.find(type => type.typeName === activeType.value);
+    if (type != null) {
+        await getFieldsForType(type.id);
+    }
     // 1: Category is empty. Change category to category of type
     if (activeCat.value == null) {
         activeCat.value = categories.value.find(cat => cat.id === type.categoryId).catName;
@@ -90,5 +108,15 @@ onMounted(async () => {
         <!-- Name text entry -->
         <v-text-field clearable label="Name"></v-text-field>
         
+        <!-- Model fields -->
+        <div class="ml-12 mr-12">
+            <v-card v-if="activeType" title="Model Fields" class="elevation-0">
+                <v-row class="text-left">
+                    <v-col v-for="(field, index) in modelFields" :key="index" cols="12" sm="4" md="4" lg="4">
+                        <v-text-field :label="field.field.name" v-model="fieldValues[index]"></v-text-field>
+                    </v-col>
+                </v-row>
+            </v-card>
+        </div>
     </div>
 </template>
