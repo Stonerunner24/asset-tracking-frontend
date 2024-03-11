b7c8ce9b65db7e1f20f42edc7e011f4c3b8f7b94
Stonerunner24 <eli.coyle1@gmail.com>
2024-03-10 20:11:42 -0500

    feat(Edit Model): Model edit is finished. A user may edit field values on models that have items associated with them, and can edit type and category of models that do not have any items associated with them.

--- src/views/ViewModel.vue
+++ src/views/ViewModel.vue
@@ -3,21 +3,25 @@ import { onMounted, ref, computed } from "vue";
 import ModelServices from "../services/modelServices";
 import CategoryServices from "../services/categoryServices";
 import TypeServices from "../services/typeServices";
+import ItemServices from "../services/itemServices";
 import {useRoute} from "vue-router";
 
 
 const model = ref([]);
 const modelFields = ref([]);
+const items = ref([]);
 const types = ref([]);
 const activeType = ref();
-const typeNames = ref([]);
+const typeNames = computed(() => types.value.map(type => type.typeName));
 const categories = ref([]);
-const catNames = ref([]);
+const catNames = computed(() => categories.value.map(category => category.catName));
 const activeCat = ref();
 
 //TODO: ITEM DATA TABLE
 
-const edit = ref(false);
+const editFields = ref(false);
+const editCatType = ref(false);
+const typeChange = ref(false);
 
 onMounted(async() => {
     const route = useRoute();
@@ -29,19 +33,25 @@ const retrieveData = async(route) => {
         //retrive models
         let response = await ModelServices.get(route.params.id);
         model.value = response.data;
-        
-        //retrieve categories
-        response = await CategoryServices.getAll();
-        categories.value = response.data;
-        activeCat.value = model.value.type.category.catName;
-        catNames.value = categories.value.map(category => category.catName);
-        console.log(catNames.value);
+        console.log(model.value);
 
-        //assign active type
-        let catId = categories.value.find(cat => cat.catName === activeCat.value).id;
-        response = await TypeServices.getAllForCategory(catId);
-        types.value = response.data;
-        typeNames.value = types.value.map(type => type.typeName);
+        response = await ItemServices.getAllForModel(route.params.id);
+        items.value = response.data;
+        console.log(items.value.length);
+
+        
+        //retrieve categories and types if there are items assigned to the model
+        if(items.value.length === 0){
+            response = await CategoryServices.getAll();
+            categories.value = response.data;
+            activeCat.value = model.value.type.category.catName;        
+
+            console.log(categories.value);
+            let catId = categories.value.find(cat => cat.catName === activeCat.value).id;
+            response = await TypeServices.getAllForCategory(catId);
+            types.value = response.data;
+        }
+        else activeCat.value = model.value.type.category.catName;  
         activeType.value = model.value.type.typeName;
 
         //retrieve model fields
@@ -51,26 +61,70 @@ const retrieveData = async(route) => {
     catch(err){
         console.error(err);
     }
-    
 };
 
-const changeCategory = () => {
+const changeCategory = async() => {
+    activeType.value = null;
+    modelFields.value = [];
+    let catId = categories.value.find(cat => cat.catName === activeCat.value).id;
+    try{
+        let response = await TypeServices.getAllForCategory(catId);
+        types.value = response.data;
+    }
+    catch(err){
+        console.error(err);
+    }
+};
 
-}
+const changeType = async() => {
+    let typeId = types.value.find(type => type.typeName === activeType.value).id;
+    try{
+        let response = await TypeServices.getAllModelFields(typeId);
+        let data = response.data;
+        console.log(data);
+        modelFields.value = [];
+        for(let field of data){
+            modelFields.value.push({
+                "value": "",
+                "fieldId": field.fieldId,
+                "field": {
+                    "name": field.field.name
+                }
+            });
+        }
+        typeChange.value = true;
+    }
+    catch(err){
+        console.error(err);
+    }
+};
 
 const changeEditMode = () => {
-    edit.value = edit.value ? false : true;
+    editFields.value = editFields.value ? false : true;
+    if(items.value.length === 0){
+        editCatType.value = editCatType.value ? false : true;
+    }
 };
 
 const cancelEdit = () => {
     window.location.reload();
-}
+};
 
 const saveChanges = async() => {
-
-
+    const data = {
+        'typeChange': typeChange.value,
+        'model': model.value,
+        'modelFields': modelFields.value
+    };
+    console.log(data);
+    try{
+        await ModelServices.update(model.value.id, data);
+    }
+    catch(err){
+        console.error(err);
+    }
     changeEditMode();
-}
+};
 
 </script>
 <template>
@@ -78,75 +132,43 @@ const saveChanges = async() => {
         <v-title class="text-h5">View Model</v-title>
     </div>
     <div class="ml-12 mr-12">
-        <v-row >
+        <v-row>
             <v-col>
-                <v-combobox
-                    chips
-                    label="Category"
-                    :disabled="!edit"
-                    v-model="activeCat"
-                    :items="catNames"
-                ></v-combobox>
+                <v-combobox chips label="Category" :disabled="!editCatType" v-model="activeCat" :items="catNames"
+                    @update:modelValue="changeCategory"></v-combobox>
                 <!-- FILTER OFF CATEGORY -->
             </v-col>
             <v-col class="text-left">
-                <v-combobox
-                    chips
-                    label="Type"
-                    :disabled="!edit"
-                    v-model="activeType"
-                    :items="typeNames"
-                    color="blue"
-                ></v-combobox>
+                <v-combobox chips label="Type" :disabled="!editCatType" v-model="activeType" :items="typeNames"
+                    color="blue" @update:modelValue="changeType"></v-combobox>
                 <!-- FILTER OFF TYPE -->
             </v-col>
-            <v-spacer></v-spacer>
-            <v-col class="text-right" v-if="edit === false">
-                <v-btn
-                    class=""
-                    color="blue"
-                    size="large"
-                    @click="changeEditMode"
-                >
+            <v-col v-if="editFields">
+                <v-text-field label="Model Name" v-model="model.model">
+
+                </v-text-field>
+            </v-col>
+            <v-spacer v-if="!editFields"></v-spacer>
+            <v-col class="text-right" v-if="editFields === false">
+                <v-btn class="" color="blue" size="large" @click="changeEditMode">
                     Edit Model
                 </v-btn>
             </v-col>
-            <v-col class="text-right" v-else-if="edit === true">
-                <v-btn
-                    class="mr-4"
-                    color="darkgray"
-                    size="large"
-                    @click="cancelEdit"
-                >
+            <v-col class="text-right" v-else-if="editFields === true">
+                <v-btn class="mr-4" color="darkgray" size="large" @click="cancelEdit">
                     Cancel
                 </v-btn>
-                <v-btn
-                    class=""
-                    color="blue"
-                    size="large"
-                    @click="saveChanges"
-                >
+                <v-btn class="" color="blue" size="large" @click="saveChanges">
                     Save Changes
                 </v-btn>
             </v-col>
         </v-row>
     </div>
     <div class="mr-12 ml-12">
-        <v-card
-            :title="model.model"
-            color="card"
-            class="pb-4"
-        >
+        <v-card :title="model.model" color="card" class="pb-4">
             <v-row class="mr-1 ml-1" v-if="modelFields">
-                <v-col
-                    v-for="m in modelFields"
-                    cols="3"
-                >
-                    <v-text-field
-                        :label="m.field.name"
-                        v-model="m.value"
-                        :disabled="!edit"
-                    ></v-text-field>
+                <v-col v-for="m in modelFields" cols="4">
+                    <v-text-field :label="m.field.name" v-model="m.value" :disabled="!editFields"></v-text-field>
                 </v-col>
             </v-row>
         </v-card>
