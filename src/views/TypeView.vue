60aec1695d2271e2a38b6c12124c86e0f4a181d2
theshaggy5000 <nickdrebot@gmail.com>
2024-03-15 13:32:27 -0500

    began setup script for TypeView.vue

--- src/views/TypeView.vue
+++ src/views/TypeView.vue
@@ -1,2 +1,28 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref, computed } from "vue";
+import { useRoute } from "vue-router";
+import TypeServices from "../services/typeServices";
+import ModelServices from "../services/modelServices";
+import ItemServices from "../services/itemServices";
+import typeServices from "../services/typeServices";
+
+const type = ref({});
+const modelsOfType = ref([]);
+
+
+onMounted(async()=>{
+    const route = useRoute();
+})
+
+const retrieveViewData = async(route) => {
+    try{
+        const response = await typeServices.get(route.params.id);
+        
+    }
+}
+
+
+
+
+</script>
 <template></template>
