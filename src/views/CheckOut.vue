d5567a69623329d7f53412a691d4d2819c15da77
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-03-16 13:04:24 -0500

    update(display start and end date): finished start and end display dates with comparer

--- src/views/CheckOut.vue
+++ src/views/CheckOut.vue
@@ -1,27 +1,69 @@
 <script setup>
     import { ref } from 'vue';
 
-    const startDate = ref(currentDate());
-    const endDate = ref('');
+    const startDatePicker = ref(null);
+    const endDatePicker = ref(null);
+    const startDate = ref(startDisplayDate());
+    const endDate = ref(endDisplayDate());
     const temp = ref(false);
+    const startDateBoole = ref(false);
+    const endDateBoole = ref(false);
 
-    function currentDate() {
-        var formatted_date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-        return formatted_date + "";
+    function startDisplayDate() {
+        if (startDatePicker.value == null) {
+            const today = new Date();
+            const month = String(today.getMonth() + 1).padStart(2, '0');
+            const day = String(today.getDate()).padStart(2, '0');
+            const year = today.getFullYear();
+            startDatePicker.value = today;
+            return `${day}/${month}/${year}`;
+        } else {
+            const date = startDatePicker.value;
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const day = String(date.getDate()).padStart(2, '0');
+            const year = date.getFullYear();
+            return `${day}/${month}/${year}`;
+        }   
+    }
+    function endDisplayDate() {
+        if (endDatePicker.value == null) {
+            return null;    
+        } else {
+            const date = endDatePicker.value;
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const day = String(date.getDate()).padStart(2, '0');
+            const year = date.getFullYear();
+            return `${day}/${month}/${year}`;
+        }   
     }
 
+    function handleStartDateChange() {
+        startDate.value = startDisplayDate();
+    }
+    function handleEndDateChange() {
+        endDate.value = endDisplayDate();
+    }
+
+    function isStartBeforeEnd(){
+        return (startDatePicker.value <= endDatePicker.value) ? true : false;
+    }
 </script>
+
 <template>
     <div class="ma-15 mt-7">
-        <div style="font-size: x-large;">Check Out</div>
-        <div>
+        <v-row>
+            <v-col><div style="font-size: x-large;">Check Out</div></v-col>
+            <v-col></v-col>
+            <v-col class="text-right"><v-btn color="blue" flat>Check Out</v-btn></v-col>
+        </v-row>
+        <div class="mt-7">
             <v-row>
                 <v-col class="text-left">
                     <v-text-field
                         variant="underlined"
                         color="blue"
                         label="Start Date"
-                        placeholder="DD/MM/YYYY"
+                        placeholder="dd/MM/yyyy"
                         v-model="startDate"
                     ></v-text-field>
                 </v-col>
@@ -29,15 +71,20 @@
                     <v-icon 
                         size="xx-large" 
                         class="hover-primary"
+                        @click="startDateBoole = !startDateBoole"
                     >mdi-calendar</v-icon>
+                    <v-overlay v-model="startDateBoole" class="align-center justify-center" contained>
+                        <v-container v-if="startDateBoole" class="datePickerDialog">
+                            <v-row justify="space-around">
+                            <v-date-picker elevation="24" v-model="startDatePicker" :picker-date-format="'dd/MM/yyyy'" @click="handleStartDateChange"></v-date-picker>
+                            </v-row>
+                        </v-container>
+                    </v-overlay> 
                 </v-col>
-                <v-col class="text-right">
-                    <v-btn color="blue" flat>Check Out</v-btn>
-                </v-col>
+                <v-col></v-col>
             </v-row>
             <v-row>
                 <v-col>
-                    <!-- ? Permanant loan instead of temporary loan -->
                     <v-checkbox
                         class="mt-n11 ml-n3"
                         color="blue"
@@ -46,15 +93,28 @@
                     ></v-checkbox>
                     <v-text-field
                         class="mt-n10"
-                        v-if="temp"
                         variant="underlined"
+                        v-if="!temp"
                         color="blue"
                         label="End Date"
-                        placeholder="DD/MM/YYYY"
+                        placeholder="dd/MM/yyyy"
                         v-model="endDate"
                     ></v-text-field>
                 </v-col>
-                <v-col></v-col>
+                <v-col v-if="!temp">
+                    <v-icon 
+                        size="xx-large" 
+                        class="hover-primary"
+                        @click="endDateBoole = !endDateBoole"
+                    >mdi-calendar</v-icon>
+                    <v-overlay v-model="endDateBoole" class="align-center justify-center" contained>
+                        <v-container v-if="endDateBoole" class="datePickerDialog">
+                            <v-row justify="space-around">
+                            <v-date-picker elevation="24" v-model="endDatePicker" :picker-date-format="'dd/MM/yyyy'" @click="handleEndDateChange"></v-date-picker>
+                            </v-row>
+                        </v-container>
+                    </v-overlay> 
+                </v-col>
                 <v-col></v-col>
             </v-row>
         </div>
