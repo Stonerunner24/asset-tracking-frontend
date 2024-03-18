cc903c250b305f10b00db447d8df29c9f3539a22
Stonerunner24 <eli.coyle1@gmail.com>
2024-03-18 16:58:38 -0400

    Update(Sidebar): added new links and tried to get the system drawer working. System drawer does not work.

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { useRoute } from 'vue-router';
+import {ref} from 'vue';
 import router from '../router';
 
 function goToHome(){
@@ -19,6 +19,8 @@ function goToModelLookup(){
     router.push("modellookup")
 }
 
+const System = ref(false);
+
 </script>
 <template>
     <div>
@@ -33,38 +35,88 @@ function goToModelLookup(){
         >
             <v-list>
                 <v-list-item
-                    prepend-icon="mdi-home-city"
+                    prepend-icon="mdi-home"
                     title="Home" 
                     v-on:click="goToHome()"
                 >
                 </v-list-item>
                 <v-list-item
-                    prepend-icon="mdi-pencil"
-                    title="Item Lookup"
+                    prepend-icon="mdi-barcode"
+                    title="Items"
                     v-on:click="goToItemLookup()"
                 >
                 </v-list-item>
+                <V-list-item
+                    prepend-icon="mdi-laptop"
+                    title="Models"
+                    v-on:click="goToModelLookup()"
+                >
+                </V-list-item>
+                <V-list-item
+                    prepend-icon="mdi-signal"
+                    title="Types"
+                    v-on:click="goToModelLookup()"
+                >
+                </V-list-item>
+                <V-list-item
+                    prepend-icon="mdi-city-variant"
+                    title="Buildings"
+                    v-on:click="goToModelLookup()"
+                >
+                </V-list-item>
+                <V-list-item
+                    prepend-icon="mdi-account"
+                    title="People"
+                    v-on:click="goToModelLookup()"
+                >
+                </V-list-item>
                 <v-list-item
                     prepend-icon="mdi-login"
                     title="Check In"
                     v-on:click="goToCheckIn()"
                 >
-
                 </v-list-item>
                 <v-list-item
                     prepend-icon="mdi-logout"
                     title="Check Out"
                     v-on:click="goToCheckOut()"
                 >
-
                 </v-list-item>
                 <V-list-item
-                    prepend-icon="mdi-file-document"
-                    title="Model Lookup"
+                    prepend-icon="mdi-wrench"
+                    title="Add Repair"
                     v-on:click="goToModelLookup()"
                 >
-
                 </V-list-item>
+                <V-list-item
+                    prepend-icon="mdi-file-document"
+                    title="Reports"
+                    v-on:click="goToModelLookup()"
+                >
+                </V-list-item>
+                <v-list-group
+                    v-model="System"
+                >
+                    <template v-slot:activator="{props}">
+                        <v-list-item
+                            title="System"
+                            prepend-icon="mdi-cog"
+                            v-bind="props"
+                        ></v-list-item>
+                    </template>
+                    <v-list-item
+                        title="Categories"
+                    ></v-list-item>
+                    <v-list-item
+                        title="Fields"
+                    ></v-list-item>
+                    <v-list-item
+                        title="Vendors"
+                    ></v-list-item>
+                    <v-list-item
+                        title="Users"
+                    ></v-list-item>
+                </v-list-group>
             </v-list>
         </v-navigation-drawer>
     </div>
