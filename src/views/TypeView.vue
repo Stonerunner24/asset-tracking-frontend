9962e9e12906febb9c9fd8331b657fa4b0879f03
theshaggy5000 <nickdrebot@gmail.com>
2024-03-17 22:58:52 -0500

    made prgress on typeView.vue. added title card, active and name boxes

--- src/views/TypeView.vue
+++ src/views/TypeView.vue
@@ -1,23 +1,34 @@
 <script setup>
 import { onMounted, ref, computed } from "vue";
 import { useRoute } from "vue-router";
-import TypeServices from "../services/typeServices";
 import ModelServices from "../services/modelServices";
 import ItemServices from "../services/itemServices";
 import typeServices from "../services/typeServices";
 
 const type = ref({});
 const modelsOfType = ref([]);
+const isActive = ref();
+const typeName = ref();
 
 
 onMounted(async()=>{
     const route = useRoute();
+    retrieveTypeData(route);
 })
 
-const retrieveViewData = async(route) => {
+const retrieveTypeData = async(route) => {
     try{
+        console.log("retrieve data for " + route.params.id);
         const response = await typeServices.get(route.params.id);
-
+        console.log("type data")
+        console.log(response.data);
+        type.value = response.data;
+        modelsOfType.value = await ModelServices.getAllByType(route.params.id);
+        console.log("models (if any)");
+        console.log(modelsOfType.value.data);
+        isActive.value = type.value.active;
+        console.log(isActive.value);
+        typeName.value = type.value.typeName;
     }
     catch(err){
         console.log(err.message);
@@ -28,4 +39,19 @@ const retrieveViewData = async(route) => {
 
 
 </script>
-<template></template>
+<template>
+    <div class="ml-8 mt-4">
+        <v-title class="text-h5">View type</v-title>
+    </div>
+    <div class="ml-12 mr-12">
+        <v-row>
+            <v-col>
+                <v-checkbox label="active" v-model="isActive" :disabled="true"></v-checkbox>
+            </v-col>
+            <v-col class="text-left">
+                <v-text-field label="name" v-model="typeName" :disabled="true"></v-text-field>
+
+            </v-col>
+        </v-row>
+    </div>
+</template>
